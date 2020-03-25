f47cb8a1b6ead0e791c98913c5289e3f578bcc40
Tracy Stark <stark.tracy@gmail.com>
2020-03-25 09:59:55 -0500

    conversion to css in js

---
+++ build/precache-manifest.3661a4ea3a6efe73bc469e7622ab01ec.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce9f4b423192307b788ba97e51c243a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99e0cccdd7e75ed2a1af",
+    "url": "/static/css/main.e6bac96f.chunk.css"
+  },
+  {
+    "revision": "928b8fe0cad9eac296c9",
+    "url": "/static/js/2.7cbe804d.chunk.js"
+  },
+  {
+    "revision": "7f3d623d03408f9350c3b5e194511d01",
+    "url": "/static/js/2.7cbe804d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99e0cccdd7e75ed2a1af",
+    "url": "/static/js/main.975b67dd.chunk.js"
+  },
+  {
+    "revision": "405f430893446efaf6a2",
+    "url": "/static/js/runtime-main.9129b20f.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
