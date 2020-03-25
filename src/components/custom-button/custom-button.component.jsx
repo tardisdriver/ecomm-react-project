e4a1cf52d318e7c84d93730878911fb9e9bcfe9d
Tracy Stark <stark.tracy@gmail.com>
2020-03-25 10:01:45 -0500

    removed scss file ref

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { CustomButtonContainer } from "./custom-button.styles";
-import "./custom-buttom.styles.scss";
 
 const CustomButton = ({ children, ...props }) => (
   <CustomButtonContainer {...props}>{children}</CustomButtonContainer>
